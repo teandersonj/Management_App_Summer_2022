a740beb374a243dfcf66cb104a8fec60ddd90f7f
TB <80381086+tbuiG@users.noreply.github.com>
2022-07-13 01:32:41 -0400

    Update new_project_menu.java

--- ManagementApp/src/new_project_menu.java
+++ ManagementApp/src/new_project_menu.java
@@ -10,6 +10,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 public class new_project_menu extends JFrame implements ActionListener {
+	
 	private JPanel contentPane;
 	private JButton aboutButton;
 	private JLabel lblNewLabel;
@@ -18,6 +19,7 @@ public class new_project_menu extends JFrame implements ActionListener {
 	
 	new_project_menu() {
 		
+		//Frame Settings
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(500,500);
 		this.setLayout(new FlowLayout());
@@ -26,25 +28,28 @@ public class new_project_menu extends JFrame implements ActionListener {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		//Title
 		lblNewLabel = new JLabel("New Project");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Segoe UI", Font.BOLD, 26));
 		lblNewLabel.setBounds(134, 36, 209, 59);
 		contentPane.add(lblNewLabel);
 		
+		//About Button
 		aboutButton = new JButton("About");
 		aboutButton.setFont(new Font("Segoe UI", Font.BOLD, 24));
 		aboutButton.setBounds(39, 122, 383, 59);
 		contentPane.add(aboutButton);
 		aboutButton.addActionListener(this);
 		
-		
+		//Project Requirements Button
 		prj_req_bttn = new JButton("Project Requirements");
 		prj_req_bttn.setFont(new Font("Segoe UI", Font.BOLD, 24));
 		prj_req_bttn.setBounds(39, 216, 383, 59);
 		contentPane.add(prj_req_bttn);
 		prj_req_bttn.addActionListener(this);
 		
+		//Project Management Button
 		prjmngbttn = new JButton("Project Management");
 		prjmngbttn.setFont(new Font("Segoe UI", Font.BOLD, 24));
 		prjmngbttn.setBounds(39, 322, 383, 59);
@@ -53,11 +58,13 @@ public class new_project_menu extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		//when About Button is clicked
 		if(e.getSource()==aboutButton) {
 			About about = new About();
 			about.setVisible(true);
 		}
 		
+		//when Project Requirements Button is clicked
 		if(e.getSource() == prj_req_bttn) {
 			project_requirements prj = new project_requirements();
 			prj.setVisible(true);
