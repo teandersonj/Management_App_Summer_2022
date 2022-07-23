27d4461f8b666801f47041d6a9574392b52ad9f5
TB <80381086+tbuiG@users.noreply.github.com>
2022-07-22 23:56:14 -0400

    Fixed Drop down menu for Risk, added Prj Req

--- ManagementApp/src/project_requirements.java
+++ ManagementApp/src/project_requirements.java
@@ -2,37 +2,70 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
 
 public class project_requirements extends JFrame implements ActionListener {
 	
 	private JPanel contentPane;
-	private JButton functionalButton;
-	private JButton nonfunctionalButton;
+	private JTextField textField;
+	private JTextField textField_1;
 	
 	project_requirements() {
 		
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.setSize(500,500);
+		this.setSize(650,700);
 		this.setLayout(new FlowLayout());
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		functionalButton = new JButton("Functional");
-		functionalButton.setFont(new Font("Segoe UI", Font.BOLD, 24));
-		functionalButton.setBounds(39, 122, 383, 59);
-		contentPane.add(functionalButton);
+		JLabel lblProjectRequirements = new JLabel("PROJECT REQUIREMENTS");
+		lblProjectRequirements.setHorizontalAlignment(SwingConstants.CENTER);
+		lblProjectRequirements.setFont(new Font("Segoe UI", Font.BOLD, 18));
+		lblProjectRequirements.setBounds(0, 11, 634, 36);
+		contentPane.add(lblProjectRequirements);
 		
-		nonfunctionalButton = new JButton("Non-Functional");
-		nonfunctionalButton.setFont(new Font("Segoe UI", Font.BOLD, 24));
-		nonfunctionalButton.setBounds(39, 216, 383, 59);
-		contentPane.add(nonfunctionalButton);
+		JLabel lblNewLabel_1 = new JLabel("Name");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Segoe UI", Font.BOLD, 16));
+		lblNewLabel_1.setBounds(49, 79, 76, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Description");
+		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_1.setFont(new Font("Segoe UI", Font.BOLD, 16));
+		lblNewLabel_1_1.setBounds(294, 76, 104, 21);
+		contentPane.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Resolved");
+		lblNewLabel_1_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_2.setFont(new Font("Segoe UI", Font.BOLD, 16));
+		lblNewLabel_1_2.setBounds(524, 75, 76, 22);
+		contentPane.add(lblNewLabel_1_2);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Segoe UI", Font.PLAIN, 12));
+		textField.setBounds(10, 113, 155, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setFont(new Font("Segoe UI", Font.PLAIN, 12));
+		textField_1.setColumns(10);
+		textField_1.setBounds(175, 113, 327, 20);
+		contentPane.add(textField_1);
+		
+		JCheckBox chckbxNewCheckBox = new JCheckBox("");
+		chckbxNewCheckBox.setHorizontalAlignment(SwingConstants.CENTER);
+		chckbxNewCheckBox.setBounds(546, 110, 32, 23);
+		contentPane.add(chckbxNewCheckBox);
 		
 	}
 	
--- ManagementApp/src/risk.java
+++ ManagementApp/src/risk.java
@@ -3,18 +3,14 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
-import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Choice;
+import java.awt.TextField;
 
 public class risk extends JFrame {
 	
 	private JPanel contentPane;
-	private JTable table;
 	private JLabel lblNewLabel;
 	private JLabel lblNewLabel_1;
 	private JLabel lblNewLabel_1_1;
@@ -43,22 +39,22 @@ public class risk extends JFrame {
 		lblNewLabel.setBounds(5, 5, 619, 36);
 		contentPane.add(lblNewLabel);
 		
-		table = new JTable();
-		table.setForeground(new Color(0, 0, 0));
-		table.setBorder(new CompoundBorder(new LineBorder(new Color(0, 0, 0)), new LineBorder(new Color(0, 0, 0))));
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{"Multilingual Support", "Certain", "Minor", "Active"},
-				{null, null, null, null},
-				{null, null, null, null},
-			},
-			new String[] {
-				"New column", "New column", "New column", "New column"
-			}
-		));
-		table.setFont(new Font("Segoe UI", Font.PLAIN, 14));
-		table.setBounds(5, 93, 606, 48);
-		contentPane.add(table);
+//		table = new JTable();
+//		table.setForeground(new Color(0, 0, 0));
+//		table.setBorder(new CompoundBorder(new LineBorder(new Color(0, 0, 0)), new LineBorder(new Color(0, 0, 0))));
+//		table.setModel(new DefaultTableModel(
+//			new Object[][] {
+//				{"Multilingual Support", "Certain", "Minor", "Active"},
+//				{null, null, null, null},
+//				{null, null, null, null},
+//			},
+//			new String[] {
+//				"New column", "New column", "New column", "New column"
+//			}
+//		));
+//		table.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+//		table.setBounds(5, 93, 606, 48);
+//		contentPane.add(table);
 		
 		lblNewLabel_1 = new JLabel("Name");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
@@ -83,5 +79,60 @@ public class risk extends JFrame {
 		lblNewLabel_1_3.setFont(new Font("Segoe UI", Font.BOLD, 16));
 		lblNewLabel_1_3.setBounds(494, 65, 76, 22);
 		contentPane.add(lblNewLabel_1_3);
+		
+		JLabel lblNewLabel = new JLabel("RISK");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Segoe UI", Font.BOLD, 18));
+		lblNewLabel.setBounds(5, 5, 619, 36);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Name");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Segoe UI", Font.BOLD, 16));
+		lblNewLabel_1.setBounds(28, 69, 76, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Likelihood");
+		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_1.setFont(new Font("Segoe UI", Font.BOLD, 16));
+		lblNewLabel_1_1.setBounds(181, 68, 104, 14);
+		contentPane.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Severity");
+		lblNewLabel_1_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_2.setFont(new Font("Segoe UI", Font.BOLD, 16));
+		lblNewLabel_1_2.setBounds(338, 65, 76, 22);
+		contentPane.add(lblNewLabel_1_2);
+		
+		JLabel lblNewLabel_1_3 = new JLabel("Status");
+		lblNewLabel_1_3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_3.setFont(new Font("Segoe UI", Font.BOLD, 16));
+		lblNewLabel_1_3.setBounds(494, 65, 76, 22);
+		contentPane.add(lblNewLabel_1_3);
+		
+		TextField textField = new TextField();
+		textField.setBounds(10, 99, 149, 22);
+		contentPane.add(textField);
+		
+		Choice choice = new Choice();
+		choice.setBounds(171, 99, 138, 22);
+		choice.add(" ");
+		choice.add("Certain");
+		choice.add("Rare");
+		contentPane.add(choice);
+		
+		Choice choice_1 = new Choice();
+		choice_1.setBounds(320, 99, 138, 20);
+		choice_1.add(" ");
+		choice_1.add("Major");
+		choice_1.add("Minor");
+		contentPane.add(choice_1);
+		
+		Choice choice_2 = new Choice();
+		choice_2.setBounds(464, 99, 138, 20);
+		choice_2.add(" ");
+		choice_2.add("Resolved");
+		choice_2.add("Active");
+		contentPane.add(choice_2);
 	}
 }
